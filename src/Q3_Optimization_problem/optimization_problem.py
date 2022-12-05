24d042b82c55031c696efa7b478c1c96322d6a31
Shlok Nangia <shlok_nangia@sfu.ca>
2022-12-05 10:52:18 -0800

    adding avg profit per order

--- src/Q3_Optimization_problem/optimization_problem.py
+++ src/Q3_Optimization_problem/optimization_problem.py
@@ -5,43 +5,30 @@ from pyspark.sql import SparkSession, functions, types
 
 # add more functions as necessary
 
-comments_schema = types.StructType([
-    types.StructField('archived', types.BooleanType()),
-    types.StructField('author', types.StringType()),
-    types.StructField('author_flair_css_class', types.StringType()),
-    types.StructField('author_flair_text', types.StringType()),
-    types.StructField('body', types.StringType()),
-    types.StructField('controversiality', types.LongType()),
-    types.StructField('created_utc', types.StringType()),
-    types.StructField('distinguished', types.StringType()),
-    types.StructField('downs', types.LongType()),
-    types.StructField('edited', types.StringType()),
-    types.StructField('gilded', types.LongType()),
-    types.StructField('id', types.StringType()),
-    types.StructField('link_id', types.StringType()),
-    types.StructField('name', types.StringType()),
-    types.StructField('parent_id', types.StringType()),
-    types.StructField('retrieved_on', types.LongType()),
-    types.StructField('score', types.LongType()),
-    types.StructField('score_hidden', types.BooleanType()),
-    types.StructField('subreddit', types.StringType()),
-    types.StructField('subreddit_id', types.StringType()),
-    types.StructField('ups', types.LongType()),
-    #types.StructField('year', types.IntegerType()),
-    #types.StructField('month', types.IntegerType()),
-])
-
-def main(inputs, output):
+def main(sales_inputs, product_inputs):
     # main logic starts here
     
-    data = spark.read.options(header='True', inferSchema='True', delimiter=',').parquet(inputs)
-    #data.show()
+    sales_data = spark.read.options(header='True', inferSchema='True', delimiter=',').parquet(sales_inputs)
+    product_data = spark.read.options(header='True', inferSchema='True', delimiter=',').csv(product_inputs)
+    #sales_data.show()
+    #product_data.show()
     #print(data.summary())
-    
+
+    product_data = product_data.withColumnRenamed('Item Number', 'Item Number_1')
+    joined_data = sales_data.join(product_data,
+        sales_data['Item Number'] == product_data['Item Number_1']
+    )
+
+    joined_data = joined_data.select(joined_data['Item Number'], joined_data['Date'], joined_data['Sale (Dollars)'], joined_data['Store Number'], joined_data['Bottles Sold'],joined_data['State Bottle Cost'])
+    joined_data.show()
+
+
     # TODO: update sales with profit(i.e sales-retail)
-    data = data.groupby(data['Item Number']).agg(functions.sum(data['Bottles Sold']).alias('Total Bottles Sold'), functions.sum(data['Sale (Dollars)']).alias('Total Sales'))
-    data_2 = data.withColumn('Profit per Bottle',  data["Total Sales"]/data["Total Bottles Sold"])
-    most_sales = data_2.sort(data["Total Bottles Sold"], ascending=False)
+    joined_data = joined_data.withColumn('Profit',  joined_data['Sale (Dollars)']-(joined_data['State Bottle Cost']*joined_data['Bottles Sold']))
+    data = joined_data.groupby(joined_data['Item Number']).agg(functions.sum(joined_data['Bottles Sold']).alias('Total Bottles Sold'), functions.sum(joined_data['Sale (Dollars)']).alias('Total Sales'), functions.avg(joined_data['Profit']).alias('Avg Profit Per Order'))
+    
+    # data_2 = data.withColumn('Profit per Bottle',  data["Total Sales"]/data["Total Bottles Sold"])
+    most_sales = data.sort(data["Total Bottles Sold"], ascending=False)
     mostSales_with_most_profit = most_sales.sort(data["Total Sales"], ascending=False)
     mostSales_with_most_profit.show()
     #data.summary()
