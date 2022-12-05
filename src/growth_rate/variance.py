bbcf707e5c027d3d4ba93db0c5531c5f66f2dc6a
Jialiang Guo <jialiang_guo@sfu.ca>
2022-12-04 23:34:05 -0800

    finished but haven't tested yet

--- src/growth_rate/variance.py
+++ src/growth_rate/variance.py
@@ -1,18 +1,12 @@
 import sys
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 import datetime
-import os
 
 from pyspark.sql import SparkSession, functions, types
 spark = SparkSession.builder.appName('total sales').getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
 assert spark.version >= '2.4' # make sure we have Spark 2.4+
 
-from pyspark.ml.pipeline import Pipeline
-from pyspark.ml.feature import StringIndexer, VectorAssembler, SQLTransformer
-from pyspark.ml.regression import GBTRegressor #DecisionTreeRegressor #RandomForestRegressor
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml import PipelineModel
 
 @functions.udf(returnType=types.StringType())
 def divide(val, col, low, high):
@@ -37,7 +31,7 @@ def load_product(inputs):
     product = spark.read.parquet(inputs+"/product") \
         .select('Item Number', 'Category Name', 'State Bottle Cost', 'Bottle Volume (ml)') \
         .cache()
-        
+    
     # divide the product into different bottle size
     size_max = product.agg(functions.max(product['Bottle Volume (ml)']).alias('max')).first()['max']
     size_min = product.agg(functions.min(product['Bottle Volume (ml)']).alias('min')).first()['min']
@@ -63,7 +57,7 @@ def load_product(inputs):
     res_product = df_size.join(df_graded, 'Item Number') \
         .select('Item Number', 'Category Name', 'Bottle Size', 'Grade')
     return res_product
-
+    
 
 def load_sale(inputs):
     # load and extract features from sale data
@@ -96,6 +90,49 @@ def main(inputs):
     # - group by the corresponding feature, further group by Year to get sum(sale) of each year
     # - for each two consecutive year compute the growth rate, and take average
     # - then compute the variance and mean of the whole thing
+    features = ['City','Category Name','Bottle Size','Grade']
+    res_tag = []
+    vars = []
+    means = []
+    
+    # for single feature
+    for i in features:
+        res_tag.append(i)
+        sub = joined.select('Year','Sale (Dollars)',i)
+        by_year = sub.groupby(i,'Year').agg(functions.sum(sub['Sale (Dollars)']).alias('sale_by_year')).cache()
+        by_year_2 = by_year
+        with_last_year = by_year.join(by_year_2, by_year['Year']==by_year_2['Year']+1) \
+            .select(by_year['Year'],by_year['Sale (Dollars)'],by_year[i],by_year_2['Sale (Dollars)'].alias('Last Year Sale'))
+        growth = with_last_year.withColumn('Growth Rate', (with_last_year['Sale (Dollars)'] - with_last_year['Last Year Sale'])/with_last_year['Last Year Sale'])
+        agg_growth = growth.groupby(i).agg(functions.mean(growth['Growth Rate']).alias('Avg Growth Rate'))
+
+        var = agg_growth.agg(functions.variance(growth['Avg Growth Rate']).alias('var')).first()['var']
+        mean = agg_growth.agg(functions.mean(growth['Avg Growth Rate']).alias('mean')).first()['mean']
+        vars.append(var)
+        means.append(mean)
+
+    # for combinations of two features
+    for i in features:
+        for j in features:
+            if i == j:
+                continue
+            res_tag.append(i+" + "+j)
+            sub = joined.select('Year','Sale (Dollars)',i, j)
+            by_year = sub.groupby(i, j,'Year').agg(functions.sum(sub['Sale (Dollars)']).alias('sale_by_year')).cache()
+            by_year_2 = by_year
+            with_last_year = by_year.join(by_year_2, by_year['Year']==by_year_2['Year']+1) \
+                .select(by_year['Year'],by_year['Sale (Dollars)'],by_year[i],by_year[j],by_year_2['Sale (Dollars)'].alias('Last Year Sale'))
+            growth = with_last_year.withColumn('Growth Rate', (with_last_year['Sale (Dollars)'] - with_last_year['Last Year Sale'])/with_last_year['Last Year Sale'])
+            agg_growth = growth.groupby(i, j).agg(functions.mean(growth['Growth Rate']).alias('Avg Growth Rate'))
+            
+            var = agg_growth.agg(functions.variance(growth['Avg Growth Rate']).alias('var')).first()['var']
+            mean = agg_growth.agg(functions.mean(growth['Avg Growth Rate']).alias('mean')).first()['mean']
+            vars.append(var)
+            means.append(mean)
+
+    print(res_tag)
+    print(vars)
+    print(means)
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
