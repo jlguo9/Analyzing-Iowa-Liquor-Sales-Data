29b5f85ff662fd0091b30d8b0ed2d5ef0cb9c4d0
Jialiang Guo <jialiang_guo@sfu.ca>
2022-12-09 12:04:06 -0800

    Update apply_update.py
    now it can receive data from kafka can feed them into cleaning pipeline

--- src/Data_Collecting/apply_update.py
+++ src/Data_Collecting/apply_update.py
@@ -8,6 +8,14 @@ import sys
 
 # from pyspark.sql import SparkSession, functions, types
 
+# flush the data to intermediate csv and fed it into data cleanng pipeline
+def data_action(lst, channel, output):
+    data = pd.DataFrame.from_records(lst)
+    itermediate_file = "./"+channel+"_tmp.csv"
+    data.to_csv(itermediate_file)
+    cmd = "python3 DataCleaning.py "+channel+" "+itermediate_file+" "+output
+    os.system(cmd)
+
 def sale(topic,output):
     consumer = KafkaConsumer(
             bootstrap_servers=['node1.local:9092', 'node2.local:9092'],
@@ -15,65 +23,96 @@ def sale(topic,output):
             value_deserializer=lambda m: json.loads(m.decode('UTF-8'))
         )
     consumer.subscribe([topic])
+    print("sale consumer is on")
+
+    # check stop signal
     t = threading.current_thread()
-    lst = []
     while getattr(t, "do_run", True):
+        lst = []
+        start_time = time.time()
         for msg in consumer:
-            lst.append(msg)
-            data = pd.DataFrame.from_records(lst)
-        data.to_csv("./sale_tmp.csv")
-        cmd = "python3 DataCleaning.py sale ./sale_tmp.csv "+output
-        os.system(cmd)
-        time.sleep(60)
+            lst.append(msg.value)
+            cur_time = time.time()
+            # flush the data every 10 seconds
+            if int(cur_time - start_time)==10:
+                data_action(lst,"sale",output)
+                start_time = cur_time
+                lst.clear()
+            # check stop signal
+            if getattr(t, "do_run", False):
+                break
+    consumer.close()
+    print("sale consumer closed")
 
 
-def store(topic):
+def store(topic,output):
     consumer = KafkaConsumer(
             bootstrap_servers=['node1.local:9092', 'node2.local:9092'],
             auto_offset_reset='earliest',
             value_deserializer=lambda m: json.loads(m.decode('UTF-8'))
         )
     consumer.subscribe([topic])
-    t = threading.current_thread()
-    lst = []
-    while getattr(t, "do_run", True):
-        for msg in consumer:
-            lst.append(msg)
-            data = pd.DataFrame.from_records(lst)
-        data.to_csv("./store_tmp.csv")
-        cmd = "python3 DataCleaning.py store ./store_tmp.csv "+output
-        os.system(cmd)
-        time.sleep(60)
+    print("store consumer is on")
 
-def product(topic):
+    # check stop signal
+    t = threading.current_thread()
+    while getattr(t, "do_run", True):
+        lst = []
+        start_time = time.time()
+        for msg in consumer:
+            lst.append(msg.value)
+            cur_time = time.time()
+            # flush the data every 10 seconds
+            if int(cur_time - start_time)==10:
+                data_action(lst,"store",output)
+                start_time = cur_time
+                lst.clear()
+            # check stop signal
+            if getattr(t, "do_run", False):
+                break
+    consumer.close()
+    print("store consumer closed")
+    
+
+def product(topic,output):
     consumer = KafkaConsumer(
             bootstrap_servers=['node1.local:9092', 'node2.local:9092'],
             auto_offset_reset='earliest',
             value_deserializer=lambda m: json.loads(m.decode('UTF-8'))
         )
     consumer.subscribe([topic])
+    print("product consumer is on")
+
+    # check stop signal
     t = threading.current_thread()
-    lst = []
     while getattr(t, "do_run", True):
+        lst = []
+        start_time = time.time()
         for msg in consumer:
-            lst.append(msg)
-            data = pd.DataFrame.from_records(lst)
-        data.to_csv("./product_tmp.csv")
-        cmd = "python3 DataCleaning.py product ./product_tmp.csv "+output
-        os.system(cmd)
-        time.sleep(60)
+            lst.append(msg.value)
+            cur_time = time.time()
+            # flush the data every 10 seconds
+            if int(cur_time - start_time)==10:
+                data_action(lst,"product",output)
+                start_time = cur_time
+                lst.clear()
+            # check stop signal
+            if getattr(t, "do_run", False):
+                break
+    consumer.close()
+    print("product consumer closed")
 
 def main(output):
-    sale_thread = threading.Thread(target=sale, args=("sale",output))
-    store_thread = threading.Thread(target=store, args=("store",output))
-    product_thread = threading.Thread(target=product, args=("product",output))
+    sale_thread = threading.Thread(target=sale, args=("sale",output,))
+    store_thread = threading.Thread(target=store, args=("store",output,))
+    product_thread = threading.Thread(target=product, args=("product",output,))
 
     sale_thread.start()
     store_thread.start()
     product_thread.start()
 
     # (for debugging) stop the threads
-    time.sleep(60)
+    time.sleep(15)
     sale_thread.do_run = False
     store_thread.do_run = False
     product_thread.do_run = False
