1b2a62158600686f15e81a1ba2038f9c4fe4cc8f
Shlok Nangia <shlok_nangia@sfu.ca>
2022-12-08 18:03:04 -0800

    updating q3

--- docs/project_report.md
+++ docs/project_report.md
@@ -37,7 +37,10 @@ Now, after obtaining the data, the next task was to clean it, extract-transform-
  1. **Brief on Data**
 
     The Iowa Alcoholic Beverages Division receives reports on wholesale transactions, and they make the data (from 2012 to the present) accessible at: [https://data.iowa.gov/](https://data.iowa.gov/Sales-Distribution/Iowa-Liquor-Sales/m3tr-qhgy).
-The collection is around 4.5 GB in size and contains 19 million transactions from 2012 to the present. 
+
+    The initial(uncleaned) was around 5.7 GB in size and contains more than 25 million transactions from 2012 to the present along with 25 columns. 
+
+    After performing Data Cleaning we were left with ~400mb of Data
 
 2. **Some Questions we intend to answer**
 
@@ -58,6 +61,7 @@ The collection is around 4.5 GB in size and contains 19 million transactions fro
      Assuming that the capital at hand is certain, how can we try to maximize the profit? I.e.  finding Capital turnover rate /Wine inventory which means The more frequently you buy, the higher the capital turnover rate (Optimization problem)
 
 
+#### Proposed Solution Design:
 ![Alt text](./project_design.png)
 
 
@@ -85,6 +89,9 @@ The collection is around 4.5 GB in size and contains 19 million transactions fro
 - Algorithmic work: Work on the algorithms needed to work with the data, including integrating data mining and machine learning techniques.
 - Bigness/parallelization: Efficiency of the analysis on a cluster, and scalability to larger data sets.
 -  UI: User interface to the results, possibly including web or data exploration frontends.
+      * We used the Github Pages to create this website to host our report and User interface to result of our analysis and Visualizations.
 - Visualization: Visualization of analysis results.
+      * We used Matplotlib python package to plot a lot of charts to demonstrate the result of our analysis, such as scatter plot, bar charts and line charts.Instances of these can be found in the report and "README.md" of each problem
 - Technologies: New technologies learned as part of doing the project. 
+    * A few technologies we learned and used while completing this project are Amazon S3, Python Spark, Amazon AWS EC2, Matplotlib, Github Pages, Kafka streamiing
 
--- src/Q3_Optimization_problem/optimization_problem.py
+++ src/Q3_Optimization_problem/optimization_problem.py
@@ -8,6 +8,29 @@ from datetime import datetime
 import matplotlib.pyplot as plot
 # add more functions as necessary
 
+
+def plot_data(joined_data, start, end):
+    joined_data = joined_data.filter(joined_data['Date']<=end)
+    joined_data = joined_data.filter(joined_data['Date']>=start)
+    joined_data = joined_data.sort(joined_data['Date'], ascending=False)
+    joined_data = joined_data.withColumn('Profit',  joined_data['Sale (Dollars)']-(joined_data['State Bottle Cost']*joined_data['Bottles Sold']))
+    data = joined_data.groupby(joined_data['Item Description']).agg(functions.sum(joined_data['Bottles Sold']).alias('Total Bottles Sold'), functions.sum(joined_data['Sale (Dollars)']).alias('Total Sales'), functions.sum(joined_data['Profit']).alias('Total Profit'))
+    
+    data = data.withColumn('Profit per Bottle',  data["Total Profit"]/data["Total Bottles Sold"])
+    most_sales = data.sort(data["Total Bottles Sold"], ascending=False)
+    mostSales_with_most_profit = most_sales.sort(data["Profit per Bottle"], ascending=False)
+    #mostSales_with_most_profit.show()
+
+    mostSales_with_most_profit_p = mostSales_with_most_profit.select(mostSales_with_most_profit['Item Description'], mostSales_with_most_profit['Profit per Bottle']).limit(5).toPandas()
+    print(mostSales_with_most_profit_p)
+    mostSales_with_most_profit_p.set_index('Item Description', inplace=True)
+    #mostSales_with_most_profit_p.plot.bar(stacked=False, legend=False, figsize=(20,10), width=.5, y='Profit per Bottle')
+    return mostSales_with_most_profit_p
+    '''plot.ylabel("Profit per Bottle")
+    plot.title(start + ' - ' + end)
+    plot.show(block=True)'''
+
+
 def main(sales_inputs, product_inputs):
     # main logic starts here
     
@@ -19,29 +42,25 @@ def main(sales_inputs, product_inputs):
     joined_data = sales_data.join(product_data,
         sales_data['Item Number'] == product_data['Item Number_1']
     )
+    print(sales_data.columns)
     
 
     joined_data = joined_data.select(joined_data['Item Number'],joined_data['Item Description'], joined_data['Date'], joined_data['Sale (Dollars)'], joined_data['Store Number'], joined_data['Bottles Sold'],joined_data['State Bottle Cost'])
-    joined_data = joined_data.filter(joined_data['Date']<='2022-10-30')
-    joined_data = joined_data.filter(joined_data['Date']>='2022-07-30')
-    joined_data = joined_data.sort(joined_data['Date'], ascending=False)
-    # joined_data.show()
-
-    joined_data = joined_data.withColumn('Profit',  joined_data['Sale (Dollars)']-(joined_data['State Bottle Cost']*joined_data['Bottles Sold']))
-    data = joined_data.groupby(joined_data['Item Description']).agg(functions.sum(joined_data['Bottles Sold']).alias('Total Bottles Sold'), functions.sum(joined_data['Sale (Dollars)']).alias('Total Sales'), functions.sum(joined_data['Profit']).alias('Total Profit'))
     
-    data = data.withColumn('Profit per Bottle',  data["Total Profit"]/data["Total Bottles Sold"])
-    most_sales = data.sort(data["Total Bottles Sold"], ascending=False)
-    mostSales_with_most_profit = most_sales.sort(data["Profit per Bottle"], ascending=False)
-    mostSales_with_most_profit.show()
-
-    mostSales_with_most_profit_p = mostSales_with_most_profit.select(mostSales_with_most_profit['Item Description'], mostSales_with_most_profit['Profit per Bottle']).limit(5).toPandas()
-    print(mostSales_with_most_profit_p)
-    mostSales_with_most_profit_p.set_index('Item Description', inplace=True)
-    mostSales_with_most_profit_p.plot.bar(stacked=False, legend=False, figsize=(20,10), width=.5, y='Profit per Bottle')
-    plot.ylabel("Profit per Bottle")
-    plot.show(block=True)
-
+    # joined_data.show()
+    dates = [{'start': '2022-07-30','end': '2022-10-30'},{'start': '2022-04-30','end': '2022-07-30'},{'start': '2022-01-30','end': '2022-04-30'}]
+    fig, axis = plot.subplots(3)
+    for i, date in enumerate(dates):
+        #print(date['start'])
+        print(i)
+        ax = plot_data(joined_data, date['start'], date['end'])
+        #axis[i].ylabel("Profit per Bottle")
+        axis[i].set_title(date['start'] + ' - ' + date['end'])
+        
+        #plot.show(block=True)
+        axis[i].plot(ax['Item Description'], ax['Profit per Bottle'])
+        
+   
     
 
 if __name__ == '__main__':
